5f8ec6d42979d4465f11dce5f086adc8e7f062b1
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-08-22 00:44:41 +0530

    Way to go

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+const user: User = new User();
+const company: Company = new Company();
+
+console.log(user);
+console.log(company);
+
+new google.maps.Map(document.getElementById("map"));
